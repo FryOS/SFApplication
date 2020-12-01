570f0f24abfd731b464a8082175f19ab08360fec
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-01 18:48:56 +0300

    Настройка проекта

---
+++ SFApplication.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SFApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Какая ваша фамилия?");
+            string surname = Console.ReadLine();
+           
+
+            Console.WriteLine($"Какой ваш возраст {name}");
+            string age = Console.ReadLine();
+            Greet(name, age);
+        }
+
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine($"Здравствуйте, {name}");
+            Console.WriteLine($"Ваш возраст {age}");
+        }
+    }
+}
