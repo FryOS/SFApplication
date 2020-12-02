8dc1602a99ab9866e2ad45c6c4c25a97d7507d08
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-02 20:53:28 +0300

    My name is Jane

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,16 +6,31 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Как вас зовут?");
-            string name = Console.ReadLine();
+            //Console.WriteLine("Как вас зовут?");
+            //string name = Console.ReadLine();
 
-            Console.WriteLine("Какая ваша фамилия?");
-            string surname = Console.ReadLine();
+            //Console.WriteLine("Какая ваша фамилия?");
+            //string surname = Console.ReadLine();
            
 
-            Console.WriteLine($"Какой ваш возраст {name}");
-            string age = Console.ReadLine();
-            Greet(name, age);
+            //Console.WriteLine($"Какой ваш возраст {name}");
+            //string age = Console.ReadLine();
+            //Greet(name, age);
+
+
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HavePet = true;
+            double shoeSize = 37.5;
+
+            Console.WriteLine($"My name is {MyName}");
+            Console.WriteLine($"My age is {MyAge}");
+            Console.WriteLine($"Do I have a pet? {HavePet}");
+            Console.WriteLine($"My shoe size is {shoeSize}");
+
+            Console.ReadKey();
+
+
         }
 
         static void Greet(string name, string age)
