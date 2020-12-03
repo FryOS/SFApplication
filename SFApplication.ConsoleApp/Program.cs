249ec00df7497c2d681d438a1181090dbf97536c
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-03 23:17:01 +0300

    3.7 Финальное занятие

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,36 +6,14 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("\tПривет,\nмир!");
-
-            //int age = 27;
-            //string name = "Jane";
-            //string favcolor = "black";
-            //Console.WriteLine("{0}\n {1} \n{2}", name, age, favcolor);
-            //Console.WriteLine($"Мой возраст {age}");
-            //Console.WriteLine("Как вас зовут?");
-            //string name = Console.ReadLine();
-
-            //Console.WriteLine("Какая ваша фамилия?");
-            //string surname = Console.ReadLine();
-
-
-            //Console.WriteLine($"Какой ваш возраст {name}");
-            //string age = Console.ReadLine();
-            //Greet(name, age);
-            //string str = Console.ReadLine();
-            //double result = 5 / 2 * 3;
-
-
-            byte Myage = checked((byte)int.Parse(Console.ReadLine()));
-            Console.WriteLine(Myage);
-        }
-
-        static void Greet(string name, string age)
-        {
-            Console.WriteLine($"Здравствуйте, {name}");
-            Console.WriteLine($"Ваш возраст {age}");
-            Console.ReadKey();            
+            Console.WriteLine($"Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Введите ваш возраст:");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваше имя {name} и возраст {age}");
+            Console.WriteLine($"Введите дату ващего рождения");
+            var birthday = Console.ReadLine();
+            Console.WriteLine($"Ваша дата {birthday}");
         }
     }
 }
