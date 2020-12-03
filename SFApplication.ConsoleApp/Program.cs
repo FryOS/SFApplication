2530e0d0fa70520777237fb4041a9988fc573f36
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-03 23:05:34 +0300

    Практика

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,22 +6,36 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Как вас зовут?");
-            string name = Console.ReadLine();
+            //Console.WriteLine("\tПривет,\nмир!");
 
-            Console.WriteLine("Какая ваша фамилия?");
-            string surname = Console.ReadLine();
-           
+            //int age = 27;
+            //string name = "Jane";
+            //string favcolor = "black";
+            //Console.WriteLine("{0}\n {1} \n{2}", name, age, favcolor);
+            //Console.WriteLine($"Мой возраст {age}");
+            //Console.WriteLine("Как вас зовут?");
+            //string name = Console.ReadLine();
 
-            Console.WriteLine($"Какой ваш возраст {name}");
-            string age = Console.ReadLine();
-            Greet(name, age);
+            //Console.WriteLine("Какая ваша фамилия?");
+            //string surname = Console.ReadLine();
+
+
+            //Console.WriteLine($"Какой ваш возраст {name}");
+            //string age = Console.ReadLine();
+            //Greet(name, age);
+            //string str = Console.ReadLine();
+            //double result = 5 / 2 * 3;
+
+
+            byte Myage = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine(Myage);
         }
 
         static void Greet(string name, string age)
         {
             Console.WriteLine($"Здравствуйте, {name}");
             Console.WriteLine($"Ваш возраст {age}");
+            Console.ReadKey();            
         }
     }
 }
