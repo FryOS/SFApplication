695d76abdeb4ef9628667711468e9bb529774b8e
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-13 17:48:33 +0300

    Твой цвет do while

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -8,28 +8,9 @@ namespace SFApplication.ConsoleApp
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            //var color = Console.ReadLine();
-
-            //if (color == "red")
-            //{
-            //    Console.BackgroundColor = ConsoleColor.Red;
-            //    Console.ForegroundColor = ConsoleColor.Black;
-            //    Console.WriteLine("Твой цвет красный");
-            //}
-            //else if (color == "green")
-            //{
-            //    Console.BackgroundColor = ConsoleColor.Green;
-            //    Console.ForegroundColor = ConsoleColor.Black;
-            //    Console.WriteLine("Твой цвет зеленый");
-            //}
-            //else
-            //{
-            //    Console.BackgroundColor = ConsoleColor.Cyan;
-            //    Console.ForegroundColor = ConsoleColor.Black;
-            //    Console.WriteLine("Твой цвет бюрюзовый");
-            //}
-
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("do while");
+            int k = 0;
+            do
             {
                 switch (Console.ReadLine().Trim().ToLower())
                 {
@@ -59,8 +40,9 @@ namespace SFApplication.ConsoleApp
                         Console.WriteLine("Your color is yellow!");
                         break;
                 }
-            }    
+                k++;
+
+            } while (k < 3);
         }
-        
     }
 }
