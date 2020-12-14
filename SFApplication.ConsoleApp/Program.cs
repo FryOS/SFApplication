0411aa9d2b51b4e4f88bce64a85a4f696abb0248
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-14 22:56:17 +0300

    Вывести Имя задом наперед

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,50 +6,12 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("do while break");
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-            int k = 0;
-            do
+            Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
+            for (int i = 1; i < name.Length+1; i++)
             {
-                var text = Console.ReadLine();
-                if (text == "stop")
-                {
-                    Console.WriteLine("Stop");
-                    break;
-                }
-
-                switch (text.Trim().ToLower())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is yellow!");
-                        break;
-                }
-                k++;
-
-            } while (k < 10);
+                Console.WriteLine(name[name.Length - i] );
+            }
         }
     }
 }
