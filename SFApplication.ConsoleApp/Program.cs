ff17f617c4f845523f771e234b7b6757d6e3d49d
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-15 23:00:23 +0300

    4.5.1 User tuple

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,22 +6,50 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            (string name, int age) = ("Alexy", 33);
+            
+            
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] FavColors, double Age) anketa;
+            Console.WriteLine("Введите Ваше имя");
+            anketa.Name = Console.ReadLine();
 
-            Console.WriteLine($"Имя {name}, возраст {age}");
+            Console.WriteLine("Введите Вашу фамилию");
+            anketa.LastName = Console.ReadLine();
 
-            (string name, int age) anketa;
-            Console.WriteLine("Введите Имя");
-            anketa.name = Console.ReadLine();
+            Console.WriteLine("Введите Ваш логин");
+            anketa.Login = Console.ReadLine();
 
-            Console.WriteLine("Введите Возраст");
-            anketa.age =  Convert.ToInt32( Console.ReadLine() );
+            anketa.LoginLength = anketa.Login.Length;
 
-            Console.WriteLine($"Имя {anketa.name}, возраст {anketa.age}");
+            anketa.HasPet = false;
+
+            Console.WriteLine("Ваши любимые цвета?");
+            anketa.FavColors = new string[3];
+           
+            for (int i = 0; i < anketa.FavColors.Length; i++)
+            {
+                var favColor = Console.ReadLine();
+                anketa.FavColors[i] = favColor;
+            }
+
+            Console.WriteLine("Введите ваш возраст");
+            anketa.Age = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Ваше имя {anketa.Name}");
+            Console.WriteLine($"Ваша фамилия {anketa.LastName}");
+            Console.WriteLine($"Ваш логин {anketa.Login}");
+            Console.WriteLine($"Колличество символом в логине {anketa.LoginLength}");
+            Console.WriteLine($"Питомец {anketa.HasPet}");
+            Console.WriteLine($"Любимые цвета:");
+            foreach (var item in anketa.FavColors)
+            {
+                Console.WriteLine($"{item}");
+            }            
+            Console.WriteLine($"Ваш возраст {anketa.Age}");
 
             Console.ReadKey();
 
 
         }
+
     }
 }
