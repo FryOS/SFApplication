e41b57a4a48b3b5323a52f918b7d2c73f7556994
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-15 23:37:01 +0300

    Готовое задание 4 модуля Анкета

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,50 +6,57 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
-            
-            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] FavColors, double Age) anketa;
-            Console.WriteLine("Введите Ваше имя");
-            anketa.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите Вашу фамилию");
-            anketa.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите Ваш логин");
-            anketa.Login = Console.ReadLine();
-
-            anketa.LoginLength = anketa.Login.Length;
-
-            anketa.HasPet = false;
-
-            Console.WriteLine("Ваши любимые цвета?");
-            anketa.FavColors = new string[3];
-           
-            for (int i = 0; i < anketa.FavColors.Length; i++)
+            for (int i = 0; i < 3; i++)
             {
-                var favColor = Console.ReadLine();
-                anketa.FavColors[i] = favColor;
+                (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] FavColors, double Age) User;
+                Console.WriteLine("Введите Ваше имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите Вашу фамилию");
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите Ваш логин");
+                User.Login = Console.ReadLine();
+
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животное: Да или Нет?");
+                var result = Console.ReadLine();
+                if (result == "Да" || result == "Yes")
+                    User.HasPet = true;
+                else
+                    User.HasPet = false;
+
+                Console.WriteLine("Ваши любимые цвета?");
+                User.FavColors = new string[3];
+
+                for (int y = 0; y < User.FavColors.Length; y++)
+                {
+                    var favColor = Console.ReadLine();
+                    User.FavColors[y] = favColor;
+                }
+
+                Console.WriteLine("Введите ваш возраст");
+                User.Age = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Ваше имя {User.Name}");
+                Console.WriteLine($"Ваша фамилия {User.LastName}");
+                Console.WriteLine($"Ваш логин {User.Login}");
+                Console.WriteLine($"Колличество символом в логине {User.LoginLength}");
+                
+                if(User.HasPet)
+                    Console.WriteLine($"Питомец есть");
+                else
+                    Console.WriteLine($"Питомца нет");
+
+                Console.WriteLine($"Любимые цвета:");
+                foreach (var item in User.FavColors)
+                {
+                    Console.WriteLine($"{item}");
+                }
+                Console.WriteLine($"Ваш возраст {User.Age}");
             }
-
-            Console.WriteLine("Введите ваш возраст");
-            anketa.Age = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Ваше имя {anketa.Name}");
-            Console.WriteLine($"Ваша фамилия {anketa.LastName}");
-            Console.WriteLine($"Ваш логин {anketa.Login}");
-            Console.WriteLine($"Колличество символом в логине {anketa.LoginLength}");
-            Console.WriteLine($"Питомец {anketa.HasPet}");
-            Console.WriteLine($"Любимые цвета:");
-            foreach (var item in anketa.FavColors)
-            {
-                Console.WriteLine($"{item}");
-            }            
-            Console.WriteLine($"Ваш возраст {anketa.Age}");
-
             Console.ReadKey();
-
-
         }
-
     }
 }
