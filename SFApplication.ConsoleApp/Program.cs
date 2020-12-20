e69964cb18ee312fa45011db31ab388a67c79947
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-20 17:51:16 +0300

    В цикле берем занчение и в массив пишем

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -8,70 +8,26 @@ namespace SFApplication.ConsoleApp
         {
             Console.WriteLine("Цикл for");
 
+            string[] favColors = new string[3];
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(i);
-                ShowColor();
+                Console.WriteLine(i);                
+                favColors[i] = ShowColor();                
             }
 
-            Console.WriteLine("Цикл while");
-            int k = 0;
-            while (k < 3)
+            Console.WriteLine("Ваши цвета");
+            for (int y = 0; y < favColors.Length; y++)
             {
-                Console.WriteLine(k);
-
-                ShowColor();
+                Console.WriteLine(favColors[y]);
             }
-
-            Console.WriteLine("Цикл do");
-            int t = 0;
-
-            do
-            {
-                Console.WriteLine(t);
-
-                ShowColor();
-
-            } while (t < 3);
-
             Console.ReadKey();
         }
 
-        private static void ShowColor()
+        static string ShowColor()
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
-
-            #region switch
-            switch (color)            
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
-            }
-            #endregion
+            return color;
         }
     }
 }
