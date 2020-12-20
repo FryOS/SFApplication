3e3a43d39f3a91dc4310cbd6b8c70b58e936b739
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-20 18:28:35 +0300

    Введенный массив - сортировка - методы

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,28 +6,49 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Цикл for");
-
-            string[] favColors = new string[3];
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine(i);                
-                favColors[i] = ShowColor();                
-            }
-
-            Console.WriteLine("Ваши цвета");
-            for (int y = 0; y < favColors.Length; y++)
-            {
-                Console.WriteLine(favColors[y]);
-            }
+            int[] arr = GetArrayFromConsole();
+            SortArray(arr);
+            WriteArray(arr);
             Console.ReadKey();
         }
 
-        static string ShowColor()
+        static int[] GetArrayFromConsole()
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-            var color = Console.ReadLine();
-            return color;
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+        static void SortArray(int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int u = 0; u < array.Length; u++)
+                {
+                    if (array[i] < array[u])
+                    {
+                        temp = array[i];
+                        array[i] = array[u];
+                        array[u] = temp;
+                    }
+                }
+            }
+        }
+
+        static void WriteArray(int[] arr)
+        {
+            Console.WriteLine("Ваши числа");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}\t");
+            }
         }
     }
 }
