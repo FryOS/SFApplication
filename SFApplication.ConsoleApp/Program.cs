6957b11bed14303c152926e9642de1023c5a3007
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-26 23:58:01 +0300

    Рекурсия Pow Степень

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,21 +6,27 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите что-то");
-            var str = Console.ReadLine();
+            //Console.WriteLine("Напишите что-то");
+            //var str = Console.ReadLine();
 
-            Console.WriteLine("Укажите глубину эха");
-            var deep = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Укажите глубину эха");
+            //var deep = int.Parse(Console.ReadLine());
 
-            Echo(str, deep);
+            //Echo(str, deep);
+
+           
+
+            int a = PowerUp(2, 3);
+            Console.WriteLine(a);
 
             Console.ReadKey();
+
         }
 
         static void Echo(string phrase, int deep)
         {
             var modif = phrase;
-            
+
             if (modif.Length > 2)
             {
                 modif = modif.Remove(0, 2);
@@ -33,5 +39,26 @@ namespace SFApplication.ConsoleApp
                 Echo(phrase, deep - 1);
             }
         }
+
+        static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+           
+        }
     }
 }
+
