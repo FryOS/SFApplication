70329f7fc3f836887ca8e674ff9ba71e7e683477
Alexey Ospishchev <alexeyosp@gmail.com>
2020-12-27 21:33:55 +0300

    Рабочая программа

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,58 +6,121 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Напишите что-то");
-            //var str = Console.ReadLine();
-
-            //Console.WriteLine("Укажите глубину эха");
-            //var deep = int.Parse(Console.ReadLine());
-
-            //Echo(str, deep);
-
-           
-
-            int a = PowerUp(2, 3);
-            Console.WriteLine(a);
+            ShowUser();
 
             Console.ReadKey();
-
         }
 
-        static void Echo(string phrase, int deep)
+        static (string Name, string LastName, bool HasPet, string[] FavColors, double Age) EnterUser()
         {
-            var modif = phrase;
+            (string Name,
+            string LastName,
+            bool HasPet,
+            string[] FavColors,
+            double Age) User;
 
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
-            Console.BackgroundColor = (ConsoleColor)deep;
-            Console.WriteLine("..." + deep);
+            Console.WriteLine("Enter your Name");
+            User.Name = Console.ReadLine();
 
-            if (deep > 1)
-            {
-                Echo(phrase, deep - 1);
-            }
-        }
+            Console.WriteLine("Enter your LastName");
+            User.LastName = Console.ReadLine();
 
-        static int PowerUp(int N, byte pow)
-        {
-            if (pow == 0)
+
+            string age;
+            int intAge;
+            do
             {
-                return 1;
+                Console.WriteLine("Enter your Age");
+                age = Console.ReadLine();
+
+            } while (!CheckNum(age, out intAge));
+
+            User.Age = intAge;
+
+
+            Console.WriteLine("Есть ли у вас животное: Да/Yes или Нет/No?");
+            var result = Console.ReadLine().ToLower();
+            if (result == "Да".ToLower() || result == "Yes".ToLower())
+            {
+                User.HasPet = true;
+                //Console.WriteLine("Сколько питомцев?");
+                string numPet;
+                int intNumPet = 0;
+                do
+                {
+                    Console.WriteLine("А Сколько питомцев?");
+                    numPet = Console.ReadLine();
+
+
+                } while (!CheckNum(numPet, out intNumPet));
+                GetNamePet(intNumPet);
+            } 
+            else if (result == "Нет".ToLower() || result == "No".ToLower())
+            {
+                Console.WriteLine("Жалко что у вас нет питомца");
+                User.HasPet = false;
             }
             else
+                User.HasPet = false;
+
+           
+            string numCol;
+            int range; 
+            do
             {
-                if (pow == 1)
+                Console.WriteLine("Какое количество ваших любимых цветов?");
+                numCol = Console.ReadLine();
+
+
+            } while (!CheckNum(numCol, out range));
+
+            User.FavColors = new string[range];
+            Console.WriteLine("Назовите их");
+            for (int y = 0; y < User.FavColors.Length; y++)
+            {
+                var favColor = Console.ReadLine();
+                User.FavColors[y] = favColor;
+            }
+
+            return User;
+        }
+
+        static bool CheckNum(string number, out int corrNumber)
+        {
+            if (int.TryParse(number, out int intnum))
+            {
+                if (intnum > 0)
                 {
-                    return N;
-                }
-                else
-                {
-                    return N * PowerUp(N, --pow);
+                    corrNumber = intnum;
+                    return true;
                 }
             }
-           
+            corrNumber = 0;
+            return false;
+        }
+
+        static string[] GetNamePet(int range)
+        {
+            string[] names = new string[range];
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Как зовут ваших питомцев");
+                string name = Console.ReadLine();
+                names[i] = name;
+            }
+            return names;
+        }
+
+        static void ShowUser()
+        {
+            var User = EnterUser();
+
+            Console.WriteLine($"Ваше имя {User.Name}");
+            Console.WriteLine($"Ваша фамилия {User.LastName}");
+            Console.WriteLine($"Ваш возраст {User.Age}");            
+            Console.WriteLine($"У вас есть питомец {User.HasPet}");
+
+            
         }
     }
 }
