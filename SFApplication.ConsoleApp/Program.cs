bcb1cdf099609ec21f6c4c0e80e68c479db47c53
Alexey Ospishchev <alexeyosp@gmail.com>
2021-01-10 17:36:04 +0300

    Result OOP exercise

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -6,126 +6,165 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
-
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
-
-            //ShowUser();
-
-            Console.ReadKey();
-        }
-
-        static (string Name, string LastName, bool HasPet, string[] FavColors, double Age) EnterUser()
-        {
-            (string Name, string LastName, bool HasPet, string[] FavColors, double Age) User;
-
-            Console.WriteLine("Enter your Name");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Enter your LastName");
-            User.LastName = Console.ReadLine();
-
-
-            string age;
-            int intAge;
-            do
-            {
-                Console.WriteLine("Enter your Age");
-                age = Console.ReadLine();
-
-            } while (!CheckNum(age, out intAge));
-
-            User.Age = intAge;
-
-
-            Console.WriteLine("Есть ли у вас животное: Да/Yes или Нет/No?");
-            var result = Console.ReadLine().ToLower();
-            if (result == "Да".ToLower() || result == "Yes".ToLower())
-            {
-                User.HasPet = true;
-                //Console.WriteLine("Сколько питомцев?");
-                string numPet;
-                int intNumPet = 0;
-                
-                do
-                {
-                    Console.WriteLine("А Сколько питомцев?");
-                    numPet = Console.ReadLine();
-
-
-                }while (!CheckNum(numPet, out intNumPet));
-            }
             
-
-            else if (result == "Нет".ToLower() || result == "No".ToLower())
-            {
-                Console.WriteLine("Жалко что у вас нет питомца");
-                User.HasPet = false;
-            }
-            else
-                User.HasPet = false;
-
-           
-            string numCol;
-            int range; 
-            do
-            {
-                Console.WriteLine("Какое количество ваших любимых цветов?");
-                numCol = Console.ReadLine();
-            } while (!CheckNum(numCol, out range));
-
-            User.FavColors = new string[range];
-            Console.WriteLine("Назовите их");
-            for (int y = 0; y < User.FavColors.Length; y++)
-            {
-                var favColor = Console.ReadLine();
-                User.FavColors[y] = favColor;
-            }
-
-            return User;
         }
 
-        static bool CheckNum(string number, out int corrNumber)
+        class MyOrders
         {
-            if (int.TryParse(number, out int intnum))
+            Order<Delivery, StructOrder>[] orders;
+
+            private Order<Delivery, StructOrder> order;
+            public MyOrders(Order<Delivery, StructOrder> order )
             {
-                if (intnum > 0)
+                orders = new Order<Delivery, StructOrder>[10];
+                this.order = order;
+            }
+
+            public Order<Delivery, StructOrder> this[int index]
+            {
+                get
                 {
-                    corrNumber = intnum;
-                    return true;
+                    return orders[index];
+                }
+                set
+                {
+                    orders[index] = value;
                 }
             }
-            corrNumber = 0;
-            return false;
+
         }
 
-        static string[] GetNamePet(int range)
+        abstract class Delivery
         {
-            string[] names = new string[range];
-            for (int i = 0; i < names.Length; i++)
+            public string Address;
+        }
+
+        abstract class Payment
+        {
+            public abstract void ToPay();
+        }
+
+        class PaymentCard : Payment
+        {
+            public override void ToPay()
             {
-                Console.WriteLine("Как зовут ваших питомцев");
-                string name = Console.ReadLine();
-                names[i] = name;
+                Console.WriteLine("Card");
             }
-            return names;
         }
 
-        static void ShowUser()
+        class PaymentCash : Payment
         {
-            var User = EnterUser();
+            public override void ToPay()
+            {
+                Console.WriteLine("Cash");
+            }
+        }
 
-            Console.WriteLine($"Ваше имя {User.Name}");
-            Console.WriteLine($"Ваша фамилия {User.LastName}");
-            Console.WriteLine($"Ваш возраст {User.Age}");            
-            Console.WriteLine($"У вас есть питомец {User.HasPet}");
+        class HomeDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class PickPointDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class ShopDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class Order<TDelivery,
+        TStruct> where TDelivery : Delivery
+            where TStruct : StructOrder
+        {
+            public TDelivery Delivery;
+            public TStruct StructOrder;
+
+            public int Number;
+
+            public string Description;
+
+            public static int countProduct; 
+
+            public void DisplayAddress()
+            {
+                Console.WriteLine(Delivery.Address);
+            }
+
+            public static int CountProduct
+            {
+                get { return countProduct; }
+                set 
+                {
+                    if(value < 0)
+                        Console.WriteLine("Пустой заказ не обслуживается");
+                    else
+                    {
+                        countProduct = value;
+                    }
+                }
+            }
+            // ... Другие поля
+        }
+
+        class StructOrder
+        {
+            private int id;
+
+            private Product productDescription;
+
+            public int ID 
+            {
+                get
+                {
+                    return id;
+                }
+                private set
+                {
+                    id = value;
+                }
+            }
+            public Product Description 
+            {
+                get
+                {
+                    return productDescription;
+                }
+                set
+                {
+                    productDescription = value;
+                }
+            }
+        }
+
+        class Product
+        {
+            private int price;
+
+            private string descprition;
+
+            public Product()
+            {
+
+            }
+
+            public Product(int priceProduct, string descpritionProduct)
+            {
+                price = priceProduct;
+                descprition = descpritionProduct;
+            }
+        }
+
+        class CategoryProduct
+        {
+            private Product product;
+
+            public CategoryProduct()
+            {
+                product = new Product();
+            }
         }
     }
 }
