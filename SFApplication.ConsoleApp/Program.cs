346245267de2495b628a2308162195621fbc88e6
Alexey Ospishchev <alexeyosp@gmail.com>
2021-01-27 20:33:52 +0300

    Unit8_Task 4

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -1,133 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace SFApplication.ConsoleApp
 {
     class Program
     {
-        const string PathTest = @"C:\Users\Alexey\source\repos\SFApplication\ToDel";
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите директорию");
-            var path = Console.ReadLine();
+            const string Path = @"C:\Users\Alexey\source\repos\SFApplication\SFApplication.ConsoleApp\Students.dat";
+            var students = DeserializeClass.DeserializeMeth<Student>(Path);
+            Directory.CreateDirectory("D:\\StudentsOsp");
 
-            if (IsItemsExists(path))
+            foreach (var s in students)
             {
-                var size = GetFolderSize(path);
-                Console.WriteLine($"Исходный размер папки {size}");
-                DeleteFolderFiles(path);
-                var sizeAfterDelete = GetFolderSize(path);
-                Console.WriteLine($"Освобождено: {size}") ;
-                Console.WriteLine($"Текущий размер папки: {sizeAfterDelete}");
-            }
-                        
-            Console.ReadLine();
-        }
-
-        static bool IsItemsExists(string path)
-        {
-            if (Directory.Exists(path))
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(path);
-
-                var folders = dirInfo.GetDirectories();
-                var files = dirInfo.GetFiles();
-                if (folders.Length == 0 && files.Length == 0)
+                if (!File.Exists($"D:\\StudentsOsp\\{s.Group}.txt"))
                 {
-                    Console.WriteLine("Элементы не найдены");
-                    return false;
-                }
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Указанный путь не найден");
-                return false;
-            }
-        }
-
-        static void DeleteFolderFiles(string path)
-        {
-            if (Directory.Exists(path))
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(path);
-
-                var folders = dirInfo.GetDirectories();
-                var files = dirInfo.GetFiles();
-                try
-                {
-                    foreach (var item in files)
+                    using (StreamWriter sw = File.CreateText($"D:\\StudentsOsp\\{s.Group}.txt"))
                     {
-                        if (item.LastAccessTime + TimeSpan.FromMinutes(0) < DateTime.Now)
-                        {
-                            item.Delete();
-                        }
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Произошла ошибка: " + ex.Message);
-                }
-
-                try
-                {
-                    foreach (var item in folders)
-                    {
-                        if (item.LastAccessTime + TimeSpan.FromMinutes(0) < DateTime.Now)
-                        {
-                            item.Delete(true);
-                            Console.WriteLine($"{item} Удален");
-                        }
-
+                        sw.WriteLine("Имя:" + s.Name + " Дата рождения:" + s.DateOfBirth);
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine("Произошла ошибка: " + ex.Message);
-                }
-            }
-        }
-
-        static long GetFolderSize(string path)
-        {
-            long size = 0;
-            if (Directory.Exists(path))
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(path);
-
-                var folders = dirInfo.GetDirectories();
-                var files = dirInfo.GetFiles();
-                try
-                {
-                    foreach (var item in files)
+                    using (StreamWriter sw = File.AppendText(($"D:\\StudentsOsp\\{s.Group}.txt")))
                     {
-                        size += item.Length;
+                        sw.WriteLine("Имя:" + s.Name + " Дата рождения:" + s.DateOfBirth);
                     }
                 }
-                catch (Exception ex)
-                {
-
-                    Console.WriteLine(ex.Message);
-                }
-
-                try
-                {
-                    foreach (var item1 in folders)
-                    {
-                        size += GetFolderSize(item1.FullName);
-                    }
-                    
-                }
-                catch (Exception ex)
-                {
-
-                    Console.WriteLine(ex.Message);
-                }
-                return size;
             }
-            else return size;
         }
     }
 }
