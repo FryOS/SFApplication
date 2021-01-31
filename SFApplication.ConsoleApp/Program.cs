0dfc537fd316406b83d7504a2797b9547efeead0
Alexey Ospishchev <alexeyosp@gmail.com>
2021-01-31 21:37:31 +0300

    Контрвариация

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace SFApplication.ConsoleApp
     class Program
     {
         public delegate Car DelHandler();
+        delegate void LexusHandlerDel(Lexus lexus);
 
         public static Car CarHandler()
         {
@@ -23,7 +24,15 @@ namespace SFApplication.ConsoleApp
         {
             DelHandler delHandler = CarHandler;
 
+            LexusHandlerDel lexusHandler = GetCarInfo;
+            lexusHandler.Invoke(new Lexus());
+
             Console.ReadLine();
+        }
+
+        public static void GetCarInfo(Car car)
+        {
+            Console.WriteLine(car.GetType());
         }
 
         private static void Method1()
