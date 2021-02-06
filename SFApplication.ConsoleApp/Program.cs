49e1460c3dad0927fac784fcee83d2000399f490
Alexey Ospishchev <alexeyosp@gmail.com>
2021-02-06 18:19:32 +0300

    Явная реализация интерфейса

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -8,104 +8,40 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Пример
-            //var lastNames = new string[5] { "Ostin", "Newel", "Patisson", "Hotter", "Wuizly" };
+            var messenger = new NewMessenger();
 
-            SortLastNameReader nameReader = new SortLastNameReader();
-            nameReader.LastNameReaderEvent += ShowNumber;
-
-            try
-            {
-                nameReader.Read();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
-            Console.WriteLine("-----------------");
-            Console.WriteLine("Просто перечисление исключений");
-            IterationExeption.IterationExeptionMethod();
+            ((IWhatsApp)messenger).SendMessage("IWhatsApp");
+            ((IWhatsApp)messenger).SendMessage("IViber");
+            
 
             Console.ReadLine();
         }
 
-        static void ShowNumber(int number, string[] arr)
-        {
-            switch (number)
-            {
-                case 1:
-                    SortLastNameReader.SortAZ(arr);
-                    Console.WriteLine("Entered 1");
-                    ShowArr(arr);  break;
-                case 2:
-                    SortLastNameReader.SortZA(arr);
-                    Console.WriteLine("Entered 2");
-                    ShowArr(arr); break;
-            }
-        }
-
-        static void ShowArr(string[] arr)
-        {
-            foreach (string s in arr)
-            {
-                Console.WriteLine(s);
-            }
-        }
+        
     }
 
 
-    public static class IterationExeption
+    public interface IWhatsApp
     {
-        public static void IterationExeptionMethod()
-        {
-            Exception[] exceptions = new Exception[]
-            {
-                    new ArgumentException(),
-                    new OverflowException(),
-                    new DirectoryNotFoundException(),
-                    new DivideByZeroException(),
-                    new MyExeption()
-
-            };
-
-            for (int i = 0; i < exceptions.Length; i++)
-            {
-                try
-                {
-                    throw exceptions[i];
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message + "\n");
-
-                }
-                catch (OverflowException ex)
-                {
-                    Console.WriteLine(ex.Message + "\n");
-                }
-                catch (DirectoryNotFoundException ex)
-                {
-                    Console.WriteLine(ex.Message + "\n");
-                }
-                catch (DivideByZeroException ex)
-                {
-                    Console.WriteLine(ex.Message + "\n");
-                }
-                catch (MyExeption ex)
-                {
-                    ex.MyExeptionMethod();
-                }
-            }
-        }
+        void SendMessage(string message);
     }
 
-    public class MyExeption : Exception
+    public interface IViber
     {
-        public void MyExeptionMethod()
+        void SendMessage(string message);
+    }
+
+    public class NewMessenger : IWhatsApp, IViber
+    {
+        void IWhatsApp.SendMessage(string message)
         {
-            Console.WriteLine("Введите цифру 1 или 2");
+            Console.WriteLine(message);
+        }
+
+        void IViber.SendMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
+
 }
