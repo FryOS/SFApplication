db019d4fb039cb2982c7053c3100362fa92c8b4a
Alexey Ospishchev <alexeyosp@gmail.com>
2021-02-18 23:30:18 +0300

    Алгоритм

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 
@@ -10,59 +11,60 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var worker = new BotWorker();
-            worker.Inizalize();
-            worker.Start();
-
-            Console.WriteLine("Напишите stop для прекращения работы");
-
-            string command;
-            do
+            var rnd = new Random();
+            Console.WriteLine("How many ? Count");
+            var count = Convert.ToInt32(Console.ReadLine());
+            User[] users = new User[count];
+            for (int i = 0; i < count; i++)
             {
-                command = Console.ReadLine();
+                var r = rnd.Next(0, 2);
+                string name = i + "name";
+                string login = i + "login";              
+                users[i] = new User(login, name, Convert.ToBoolean(r));
+            }
 
-            } while (command != "stop");
-
-            worker.Stop();
-
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"Hello {users[i].Name}");
+                if (users[i].IsPremium == false)
+                    ShowAds(users[i].Name);
+                else
+                    Console.WriteLine($"Привет, {users[i].Name} у вас куплена подписка");
+            }
+            
             Console.ReadLine();
         }
-    }
 
-    public class BotWorker
-    {
-        private ITelegramBotClient botClient;
 
-        public void Inizalize()
+        static void ShowAds(string name)
         {
-            botClient = new TelegramBotClient(BotCredentials.BotToken);
-        }
+            Console.WriteLine();
+            Console.WriteLine($"Привет {name}");
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
 
-        public void Start()
-        {
-            botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving();
-        }
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
 
-        public void Stop()
-        {
-            botClient.StopReceiving();
-        }
-
-
-        private async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text != null)
-            {
-                await botClient.SendTextMessageAsync(
-                chatId: e.Message.Chat, text: "Вы написали:\n" + e.Message.Text);
-            }
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 
-
-    class BotCredentials
+    class User
     {
-        public static readonly string BotToken = "1534517938:AAEYzj1egh9SvZyh4PYU0t_mhmW5uhh_hS8";
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremiun)
+        {
+            this.Login = login;
+            this.Name = name;
+            this.IsPremium = isPremiun;
+        }
     }
 }
