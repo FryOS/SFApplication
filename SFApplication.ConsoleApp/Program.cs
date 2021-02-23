1db1359f227630449f7322144e97244f034b1d80
Alexey Ospishchev <alexeyosp@gmail.com>
2021-02-23 20:17:11 +0300

    Подсчет кол-ва сло в файле

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -11,60 +11,16 @@ namespace SFApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var rnd = new Random();
-            Console.WriteLine("How many ? Count");
-            var count = Convert.ToInt32(Console.ReadLine());
-            User[] users = new User[count];
-            for (int i = 0; i < count; i++)
-            {
-                var r = rnd.Next(0, 2);
-                string name = i + "name";
-                string login = i + "login";              
-                users[i] = new User(login, name, Convert.ToBoolean(r));
-            }
+            // читаем весь файл в строку 
+            string text = File.ReadAllText(@"C:\Users\Alexey\source\repos\SFApplication\SFApplication.ConsoleApp\Text.txt");
+            // разбиваем в массив, используя пробел в качестве разделителя
+            var words = text.Split(new char[] { ' ' }).Length;
 
-            for (int i = 0; i < users.Length; i++)
-            {
-                Console.WriteLine($"Hello {users[i].Name}");
-                if (users[i].IsPremium == false)
-                    ShowAds(users[i].Name);
-                else
-                    Console.WriteLine($"Привет, {users[i].Name} у вас куплена подписка");
-            }
-            
+            // выводим количество
+            Console.WriteLine(words);
+
+
             Console.ReadLine();
-        }
-
-
-        static void ShowAds(string name)
-        {
-            Console.WriteLine();
-            Console.WriteLine($"Привет {name}");
-            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-            // Остановка на 1 с
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-            // Остановка на 2 с
-            Thread.Sleep(2000);
-
-            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-            // Остановка на 3 с
-            Thread.Sleep(3000);
-        }
-    }
-
-    class User
-    {
-        public string Login { get; set; }
-        public string Name { get; set; }
-        public bool IsPremium { get; set; }
-
-        public User(string login, string name, bool isPremiun)
-        {
-            this.Login = login;
-            this.Name = name;
-            this.IsPremium = isPremiun;
         }
     }
 }
