5302835d8cb63ba7037a9744b77bcec08fc8fb43
Alexey Ospishchev <alexeyosp@gmail.com>
2021-02-23 23:42:40 +0300

    List and UniqContact

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -19,8 +20,81 @@ namespace SFApplication.ConsoleApp
             // выводим количество
             Console.WriteLine(words);
 
+            var months = new[]
+            {
+               "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            var numbers = new[]
+            {
+               1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+
+            // инициализация ArrayList
+            var combinedList = new ArrayList();
+
+            //  пробегаемся по массиву чисел
+            foreach (var number in numbers)
+            {
+                // добавляем в ArrayList строку месяца (начинаем с нулевого по индексу)
+                combinedList.Add(months[number - 1]);
+
+                // добавляем его порядковый номер
+                combinedList.Add(number);
+            }
+
+            //foreach (var value in combinedList)
+            //    Console.WriteLine(value);
+            
+            var phoneBook = new List<Contact>();
+            AddUnique(new Contact("alex", 8888, "alex@oa.ru"), phoneBook);
+
+            
+            
 
             Console.ReadLine();
+
+        }
+
+        static void AddUnique(Contact newContact, List<Contact> phoneBook)
+        {
+            bool alreadyExists = false;
+
+            // пробегаемся по списку и смотрим, есть ли уже с таким именем
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == newContact.Name)
+                {
+                    //  если вдруг находим  -  выставляем флаг и прерываем цикл
+                    alreadyExists = true;
+                    break;
+                }
+            }
+
+            if (!alreadyExists)
+                phoneBook.Add(newContact);
+
+            //  сортируем список по имени
+            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+        }
+
+
+
+        public class Contact // модель класса
+        {
+            public Contact(string name, long phoneNumber, String email) // метод-конструктор
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public String Name { get; }
+            public long PhoneNumber { get; }
+            public String Email { get; }
         }
     }
 }
