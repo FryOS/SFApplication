00ae65f69a5bb2b347033e8463970bde079bc6b6
Alexey Ospishchev <alexeyosp@gmail.com>
2021-03-09 23:03:37 +0300

    Unit14_Task1

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -2,40 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 
-
-
 namespace SFApplication.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var objects = new List<object>()
-                {
-                   1,
-                   "Сергей ",
-                   "Андрей ",
-                   300,
-                };
-
-            var list = from p in objects
-                       where p is string
-                       orderby p
-                        select p;
-            foreach (var item in list)
+            var contacts = new List<Contact>()
             {
-                Console.WriteLine(item) ;
-            }
+               new Contact() { Name = "Андрей", Phone = 7999945059, Age = 23 },
+               new Contact() { Name = "Сергей", Phone = 7999945052, Age = 20 },
+               new Contact() { Name = "Иван", Phone = 7999945051 , Age = 21},
+               new Contact() { Name = "Игорь", Phone = 7999945055, Age = 32 },
+               new Contact() { Name = "Анна", Phone = 7999945057, Age = 40 },
+               new Contact() { Name = "Василий", Phone = 7999945058, Age = 19 }
+            };
 
-            foreach (var item in objects.Where(p => p is string).OrderBy(p => p))
-            {
-                Console.WriteLine(item);
-            }
+            var sortContacts = contacts.OrderBy(c => c.Name).ThenBy(c => c.Age);
+            foreach (var item in sortContacts)
+                Console.WriteLine($"{item.Name} {item.Age}");
 
             Console.ReadLine();
-
         }
-
-         
+    }
+    
+    public class Contact
+    {
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public int Age { get; set; }
     }
 }
---
+++ SFApplication.ConsoleApp/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFApplication.ConsoleApp
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private List<string> languages;
+
+        public List<string> Languages
+        {
+            get { return languages; }
+            set { languages = value; }
+        }
+    }
+}
