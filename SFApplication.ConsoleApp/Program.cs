ed587e23c841590d785832cac511d9edfd286147
Alexey Ospishchev <alexeyosp@gmail.com>
2021-03-11 23:18:37 +0300

    Настройка проекта для теста

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -8,28 +8,11 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            var classes = new[]
-            {
-               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
-               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
-               new Classroom { Students = {"Bulat", "Alex", "Galina"}, },
-               new Classroom { Students = {"Ramis", "Alexey", "Polina"}, }
-           };
-            var allStudents = GetAllStudents(classes);
-
-            Console.WriteLine(string.Join(" ", allStudents));
+           
             Console.ReadKey();
         }
 
-        static string[] GetAllStudents(Classroom[] classes)
-        {
-            return classes.SelectMany(c => c.Students).ToArray();
-        }
-
-        public class Classroom
-        {
-            public List<string> Students = new List<string>();
-        }
+        
     }
 }
 
