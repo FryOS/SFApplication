5e3c032ad050bd511889137da3d8eeae02b511e6
Alexey Ospishchev <alexeyosp@gmail.com>
2021-03-13 12:40:28 +0300

    Add folder Tests

--- SFApplication.ConsoleApp/Program.cs
+++ SFApplication.ConsoleApp/Program.cs
@@ -89,6 +89,28 @@ namespace Task1
                 return ticketService.GetTicketPrice(ticketId);
             }
         }
+
+        public class UserRepository : IUserRepository
+        {
+            public IEnumerable<User> FindAll()
+            {
+                return null;
+            }
+        }
+
+        public interface IUserRepository
+        {
+            IEnumerable<User> FindAll();
+        }
+
+        public class User
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+        }
     }
 }
 
